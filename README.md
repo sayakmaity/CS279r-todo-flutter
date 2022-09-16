415160e7a4437fe2d6a5316614d59925b75943d3
Sayak Maity <smaity@college.harvard.edu>
2022-09-16 17:49:14 -0400

    fixed

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
-Assuming you have flutter installed, cd into the main directory and run ```flutter run``` in the terminal. Then select option 2 in the terminal for Chrome. 
+Assuming you have flutter installed, cd into the main directory and run ```flutter run``` in the terminal. 
 
 Tutorial source: 
-https://flutterawesome.com/a-simple-to-use-todo-app-build-with-the-flutter-framework/
-https://github.com/AgweBryan/flutter_todo_app
+https://github.com/rebelchris/flutter/tree/todolist
--- lib/main.dart
+++ lib/main.dart
@@ -1,50 +1,127 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:flutter_todo_app/constants/theme.dart';
-import 'package:flutter_todo_app/controllers/theme_controller.dart';
-import 'package:flutter_todo_app/providers/database_provider.dart';
-import 'package:flutter_todo_app/views/screens/home_screen.dart';
-import 'package:get/get.dart';
-import 'package:get_storage/get_storage.dart';
 
-/// initializes the database, shared preferences, and screen size
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await DatabaseProvider.initDb();
-  await GetStorage.init();
-  // await ScreenUtil.ensureScreenSize();
-
-  // SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
-  runApp(
-    MyApp(),
-  );
+/// defines Todo class
+class Todo {
+  Todo({required this.name, required this.checked});
+  final String name;
+  bool checked;
 }
 
-/// main method to run the app
-class MyApp extends StatelessWidget {
-  MyApp({Key? key}) : super(key: key);
+class TodoItem extends StatelessWidget {
+  TodoItem({
+    required this.todo,
+    required this.onTodoChanged,
+  }) : super(key: ObjectKey(todo));
 
-  final ThemeController _themeController = Get.put(ThemeController());
+  final Todo todo;
+  final onTodoChanged;
+
+  TextStyle? _getTextStyle(bool checked) {
+    if (!checked) return null;
+
+    return TextStyle(
+      color: Colors.black54,
+      decoration: TextDecoration.lineThrough,
+    );
+  }
 
   @override
-  
   Widget build(BuildContext context) {
-    return ScreenUtilInit(
-      designSize: const Size(360, 690),
-      minTextAdapt: true,
-      splitScreenMode: true,
-      builder: (context, child) {
-        return GetMaterialApp(
-          debugShowCheckedModeBanner: false,
-          title: 'Flutter TODO App',
-          theme: Themes.lightTheme,
-          themeMode: _themeController.theme,
-          darkTheme: Themes.darkTheme,
-          home: child,
-        );
+    return ListTile(
+      onTap: () {
+        onTodoChanged(todo);
       },
-      child: HomeScreen(),
+      leading: CircleAvatar(
+        child: Text(todo.name[0]),
+      ),
+      title: Text(todo.name, style: _getTextStyle(todo.checked)),
     );
   }
 }
+
+class TodoList extends StatefulWidget {
+  @override
+  _TodoListState createState() => new _TodoListState();
+}
+
+/// Defines widget that displays a list of todo items, and allows the user to add new todo items
+class _TodoListState extends State<TodoList> {
+  final TextEditingController _textFieldController = TextEditingController();
+  final List<Todo> _todos = <Todo>[];
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('Todo list'),
+      ),
+      body: ListView(
+        padding: EdgeInsets.symmetric(vertical: 8.0),
+        children: _todos.map((Todo todo) {
+          return TodoItem(
+            todo: todo,
+            onTodoChanged: _handleTodoChange,
+          );
+        }).toList(),
+      ),
+      floatingActionButton: FloatingActionButton(
+          onPressed: () => _displayDialog(),
+          tooltip: 'Add Item',
+          child: Icon(Icons.add)),
+    );
+  }
+
+  void _handleTodoChange(Todo todo) {
+    setState(() {
+      todo.checked = !todo.checked;
+    });
+  }
+
+  /// adds todos to the list
+  void _addTodoItem(String name) {
+    setState(() {
+      _todos.add(Todo(name: name, checked: false));
+    });
+    _textFieldController.clear();
+  }
+
+/// shows a dialog with a text field and an "Add" button. When the user taps the "Add"
+/// button, the dialog is dismissed and the text field's text is added to the list of todo items
+
+  Future<void> _displayDialog() async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false, // user must tap button!
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Add a new todo item'),
+          content: TextField(
+            controller: _textFieldController,
+            decoration: const InputDecoration(hintText: 'Type your new todo'),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('Add'),
+              onPressed: () {
+                Navigator.of(context).pop();
+                _addTodoItem(_textFieldController.text);
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
+
+class TodoApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Todo list',
+      home: new TodoList(),
+    );
+  }
+}
+
+void main() => runApp(new TodoApp());
